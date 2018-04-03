ffd8b0cf6613df71f570302bc8b259f2bbd55032
Edwin Mendez <emendez@gravitalsoftware.com>
2018-04-02 23:43:50 -0500

    models makemigrations - migrate

--- project/app_module/views.py
+++ project/app_module/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import Http404
+
 
 # Create your views here.
+#def hello_view(request):
+#    musics = Music.objects.all()
+#    # raise Exception('error !!!!')
+#    # raise Http404("sorry 404")
+#    return render(request, 'hello_django.html', {
+#        'data': "Hello Django ",
+#        'musics': musics,
+#    })
